945839afe3bca6760cf982550f729111b55a156d
realaxiompeptides <realaxiompeptides@gmail.com>
2026-03-11 19:57:38 -0700

    Create homepage-collection-data.js

---
+++ homepage/homepage-collection-data.js
@@ -0,0 +1,34 @@
+window.HOMEPAGE_COLLECTION_PRODUCTS = [
+  {
+    name: "GLP-3 RT (5-40MG)",
+    price: "$50.00 – $185.00",
+    image: "images/product-1.PNG",
+    badge: "-23%",
+    buttonText: "Select Options",
+    link: "product-glp3rt.html"
+  },
+  {
+    name: "GHK-CU (50-100MG)",
+    price: "$35.00 – $45.00",
+    image: "images/product-2.PNG",
+    badge: "-36%",
+    buttonText: "Select Options",
+    link: "product-ghkcu.html"
+  },
+  {
+    name: "BPC-157 (5MG)",
+    price: "$29.00 – $39.00",
+    image: "images/product-3.PNG",
+    badge: "Popular",
+    buttonText: "Select Options",
+    link: "product-bpc157.html"
+  },
+  {
+    name: "TB-500 (10MG)",
+    price: "$45.00 – $65.00",
+    image: "images/product-4.PNG",
+    badge: "Top Pick",
+    buttonText: "Select Options",
+    link: "product-tb500.html"
+  }
+];
