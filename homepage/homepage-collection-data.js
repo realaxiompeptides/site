a97bd471af937246b1301709f391fe10b9d09a3a
realaxiompeptides <realaxiompeptides@gmail.com>
2026-03-13 10:02:06 -0700

    Update homepage-collection-data.js

--- homepage/homepage-collection-data.js
+++ homepage/homepage-collection-data.js
@@ -1,30 +1,12 @@
 window.HOMEPAGE_COLLECTION_PRODUCTS = [
   {
     name: "GLP-3 RT",
-    oldPrice: "",
-    price: "$20.00",
+    oldPrice: "$45.00",
+    price: "$39.00",
     image: "images/products/reta-5mg-main.PNG",
     badge: "SALE",
     buttonText: "View Product",
-    link: "product-page/product.html?slug=glp3-rt"
-  },
-  {
-    name: "TB-500",
-    oldPrice: "",
-    price: "$22.00",
-    image: "images/products/tb-500-5mg-main.PNG",
-    badge: "SALE",
-    buttonText: "View Product",
-    link: "product-page/product.html?slug=tb-500"
-  },
-  {
-    name: "BPC-157",
-    oldPrice: "",
-    price: "$18.00",
-    image: "images/products/bpc-157-5mg-main.PNG",
-    badge: "SALE",
-    buttonText: "View Product",
-    link: "product-page/product.html?slug=bpc-157"
+    link: "product-page/product.html?slug=glp-3-rt"
   },
   {
     name: "GHK-CU",
@@ -36,13 +18,31 @@ window.HOMEPAGE_COLLECTION_PRODUCTS = [
     link: "product-page/product.html?slug=ghk-cu"
   },
   {
+    name: "BPC-157",
+    oldPrice: "$35.00",
+    price: "$25.00",
+    image: "images/products/bpc-157-5mg-main.PNG",
+    badge: "SALE",
+    buttonText: "View Product",
+    link: "product-page/product.html?slug=bpc-157"
+  },
+  {
+    name: "TB-500",
+    oldPrice: "$45.00",
+    price: "$36.00",
+    image: "images/products/tb-500-5mg-main.PNG",
+    badge: "SALE",
+    buttonText: "View Product",
+    link: "product-page/product.html?slug=tb-500"
+  },
+  {
     name: "IPAMORELIN",
     oldPrice: "",
     price: "$18.00",
     image: "images/products/ipa-5mg-main.PNG",
     badge: "SALE",
     buttonText: "View Product",
-    link: "product-page/product.html?slug=ipa"
+    link: "product-page/product.html?slug=ipamorelin"
   },
   {
     name: "CJC 1295 NO DAC",
