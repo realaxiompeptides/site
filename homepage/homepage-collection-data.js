47b62693c0b797686fbe3faef1d5182ec05cd4a2
realaxiompeptides <realaxiompeptides@gmail.com>
2026-03-13 09:27:33 -0700

    Update homepage-collection-data.js

--- homepage/homepage-collection-data.js
+++ homepage/homepage-collection-data.js
@@ -1,38 +1,83 @@
 window.HOMEPAGE_COLLECTION_PRODUCTS = [
   {
     name: "GLP-3 RT",
-    oldPrice: "$45.00",
-    price: "$39.00",
+    oldPrice: "",
+    price: "$20.00",
     image: "images/products/reta-5mg-main.PNG",
-    badge: "SALE",
+    badge: "",
     buttonText: "View Product",
-    link: "product.html?slug=glp3-rt"
+    link: "product-page/product.html?slug=glp3-rt"
+  },
+  {
+    name: "TB-500",
+    oldPrice: "",
+    price: "$22.00",
+    image: "images/products/tb-500-5mg-main.PNG",
+    badge: "",
+    buttonText: "View Product",
+    link: "product-page/product.html?slug=tb-500"
+  },
+  {
+    name: "BPC-157",
+    oldPrice: "",
+    price: "$18.00",
+    image: "images/products/bpc-157-5mg-main.PNG",
+    badge: "",
+    buttonText: "View Product",
+    link: "product-page/product.html?slug=bpc-157"
   },
   {
     name: "GHK-CU",
     oldPrice: "",
-    price: "$30.00 – $43.00",
+    price: "$11.00 – $20.00",
     image: "images/products/ghk-cu-100mg-main.PNG",
-    badge: "SALE",
+    badge: "",
     buttonText: "View Product",
-    link: "product.html?slug=ghk-cu"
+    link: "product-page/product.html?slug=ghk-cu"
   },
   {
-    name: "BPC-157",
-    oldPrice: "$35.00",
-    price: "$25.00",
-    image: "images/products/bpc-157-5mg-main.PNG",
-    badge: "SALE",
+    name: "Ipamorelin",
+    oldPrice: "",
+    price: "$18.00",
+    image: "images/products/ipa-5mg-main.PNG",
+    badge: "",
     buttonText: "View Product",
-    link: "product.html?slug=bpc-157"
+    link: "product-page/product.html?slug=ipa"
   },
   {
-    name: "TB-500",
-    oldPrice: "$45.00",
-    price: "$36.00",
-    image: "images/products/tb-500-5mg-main.PNG",
-    badge: "SALE",
+    name: "CJC-1295 (No DAC)",
+    oldPrice: "",
+    price: "$20.00",
+    image: "images/products/cjc-1295-nodac-5mg-main.PNG",
+    badge: "",
     buttonText: "View Product",
-    link: "product.html?slug=tb-500"
+    link: "product-page/product.html?slug=cjc-1295-no-dac"
+  },
+  {
+    name: "MT-2",
+    oldPrice: "",
+    price: "$15.00",
+    image: "images/products/mt-2-10mg-main.PNG",
+    badge: "",
+    buttonText: "View Product",
+    link: "product-page/product.html?slug=mt-2"
+  },
+  {
+    name: "MOTS-C",
+    oldPrice: "",
+    price: "$18.00",
+    image: "images/products/mots-c-10mg-main.PNG",
+    badge: "",
+    buttonText: "View Product",
+    link: "product-page/product.html?slug=mots-c"
+  },
+  {
+    name: "BAC Water (10ml)",
+    oldPrice: "",
+    price: "$10.00",
+    image: "images/products/bac-water-10ml-main.PNG",
+    badge: "",
+    buttonText: "View Product",
+    link: "product-page/product.html?slug=bac-water"
   }
 ];
