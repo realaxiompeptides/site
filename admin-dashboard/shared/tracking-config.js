7cca55afbcf6bf02b96d694bf2484c170ebc4fd4
realaxiompeptides <realaxiompeptides@gmail.com>
2026-03-16 00:01:22 -0700

    Create tracking-config.js

---
+++ admin-dashboard/shared/tracking-config.js
@@ -0,0 +1,8 @@
+window.AXIOM_TRACKING_CONFIG = {
+  STORAGE_KEYS: {
+    VISITOR_ID: "axiom_visitor_id",
+    CHECKOUT_SESSION_ID: "axiom_checkout_session_id",
+    CART: "axiom_cart"
+  },
+  ORDER_NUMBER_START: 1000
+};
